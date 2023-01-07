9d2d1b2211ae8180e217f9e8326ce49db3916a1d
maarioherns21 <maarioherns21@gmail.com>
2023-01-06 18:34:50 -0800

    adding Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 
 
-const Footer = () =>{
 
-    return (
-        <div>Footer</div>
-    )
-}
+const Footer = () => (
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+        © 2023 All rights reserved by Mario Hernandez
+      </p>
+    </div>
+  );
 
 export default Footer
