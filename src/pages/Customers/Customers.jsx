79f2fbbe0568be3a391e0a23eb80a5c4f456cb2f
maarioherns21 <maarioherns21@gmail.com>
2023-01-06 18:33:11 -0800

    adding syncfusin Orders,Employess,Comstumers

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -1,11 +1,34 @@
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
 
+import { customersData, customersGrid } from '../../data/dummy';
+import Header from "../../components/Header/Header"
 
+const Customers = () => {
+  const selectionsettings = { persistSelection: true };
+  const toolbarOptions = ['Delete', 'Search'];
+  const editing = { allowDeleting: true, allowEditing: true };
 
-const Customers = () =>{
-
-    return (
-        <div>Customers</div>
-    )
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        enableHover={false}
+        allowPaging
+        pageSettings={{ pageCount: 5 }}
+        selectionSettings={selectionsettings}
+        toolbar={toolbarOptions}
+        editSettings={editing}
+        allowSorting
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 }
 
 export default Customers
